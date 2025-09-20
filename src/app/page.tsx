6a7cd4c3f07a6667429a601e4e8b996cbe07af18
alvaroprs8 <alvaroprs@prsacademy.com.br>
2025-09-20 18:19:31 -0300

    fix(build): remove duplicate redirect/import in app/page.tsx and keep /deliverables redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
 import { redirect } from 'next/navigation'
 
-export default function Home() {
-  redirect('/deliverables')
-}
-
-import { redirect } from 'next/navigation'
-
 export const dynamic = 'force-dynamic'
 
-export default function HomeRedirect() {
-  redirect('/lead')
+export default function Home() {
+  redirect('/deliverables')
 }
 
 
